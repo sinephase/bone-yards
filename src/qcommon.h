9c1f7681b386173d646382b23f42f0daf27332c9
sinephase <briansherred@gmail.com>
2026-08-23 04:42:11 -0400

    feat: add common header file

---
+++ src/qcommon.h
@@ -0,0 +1,51 @@
+// qcommon.h -- common definitions and declarations
+
+#ifndef QCOMMON_H
+#define QCOMMON_H
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Platform detection
+#ifdef _WIN32
+    #define PLATFORM_WINDOWS 1
+#elif __APPLE__
+    #define PLATFORM_MACOS 1
+#elif __linux__
+    #define PLATFORM_LINUX 1
+#endif
+
+// Basic types
+typedef uint8_t byte;
+typedef uint16_t word;
+typedef uint32_t dword;
+typedef int32_t qboolean;
+
+#define true 1
+#define false 0
+
+// Entity structure
+struct edict_t {
+    int s_modelindex;
+    float origin[3];
+    float angles[3];
+    float velocity[3];
+    float scale;
+    
+    // Simplified structure - extend as needed
+    struct {
+        int modelindex;
+    } s;
+};
+
+// Console functions
+void Com_Printf(const char* fmt, ...);
+void Com_DPrintf(const char* fmt, ...);
+void Com_Error(int level, const char* fmt, ...);
+void Com_Warning(const char* fmt, ...);
+unsigned int Com_Milliseconds();
+void Com_Init();
+void Com_Shutdown();
+
+#endif // QCOMMON_H
