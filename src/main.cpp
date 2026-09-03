76e9e92b7a442e29064b3c9101f887343da6adf9
sinephase <briansherred@gmail.com>
2026-09-03 14:06:10 -0400

    fix: switch back to main() for standard entry point

--- src/main.cpp
+++ src/main.cpp
@@ -142,8 +142,8 @@ void Main_Shutdown() {
     Com_Printf("Goodbye!\n");
 }
 
-// Entry point - using wmain for Windows with proper C++ linkage
-int wmain(int argc, wchar_t* argv[]) {
+// Entry point
+int main(int argc, char* argv[]) {
     Com_Init();
 
     if (!Main_Init()) {
